0870bd70f74c344901d9a16b3ab84fb263ee1501
Todd Anderson <bustardcelly@gmail.com>
2023-05-23 11:54:36 -0400

    allowing for programmatically injecting embed.

---
+++ r5pro_rtmv_iframe.js
@@ -0,0 +1,57 @@
+const baseConfig = {
+  embedderBase: 'http://127.0.0.1:5173',
+  host: window.location.origin,
+  app: 'live',
+  abr: false,
+  abrLow: 3,
+  abrHigh: 1,
+  debug: false,
+  scriptUrl: undefined,
+  streamList: [], // [{ label: <string>, streamName: <string>}]
+}
+
+class RTMV {
+  constructor(element, config) {
+    let configuration = { ...baseConfig, ...config }
+    let elementToReplace =
+      typeof element === 'string' ? document.getElementById(element) : element
+    if (elementToReplace && elementToReplace.parentNode) {
+      let iframe = document.createElement('iframe')
+      iframe.src = RTMV.generateIFrameURL(configuration)
+      iframe.id = 'r5pro-rtmv-iframe'
+      iframe.width = config.width || '100%'
+      iframe.height = config.height || '100%'
+      elementToReplace.parentNode.replaceChild(iframe, elementToReplace)
+    } else {
+      console.error(
+        `Could not find element or parent for: ${
+          elementToReplace ? elementToReplace.id : element
+        }.`
+      )
+    }
+  }
+
+  static generateIFrameURL(config) {
+    let url = new URL(config.embedderBase)
+    url.pathname = '/embed'
+    url.searchParams.set('host', config.host)
+    url.searchParams.set('app', config.app)
+    url.searchParams.set('abr', config.abr)
+    url.searchParams.set('abrlow', config.abrLow)
+    url.searchParams.set('abrhigh', config.abrHigh)
+    url.searchParams.set('debug', config.debug)
+    if (config.url) {
+      url.searchParams.set('url', config.scriptUrl)
+    } else if (config.streamList && config.streamList.length > 0) {
+      config.streamList.forEach((stream) => {
+        url.searchParams.set(stream.label, stream.streamName)
+      })
+    }
+    return url.toString()
+  }
+}
+
+window.R5PRO = {
+  RTMV,
+}
+window.onR5ProReady(window.R5PRO)
